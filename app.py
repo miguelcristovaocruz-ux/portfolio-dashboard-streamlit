16369ea5578c866f106bf9ce2c42304b48d730fa
miguelcristovaocruz-ux <miguelcristovaocruz@gmail.com>
2025-11-07 16:54:33 -0300

    7/11 v22

--- app.py
+++ app.py
@@ -183,7 +183,7 @@ def fetch_prices_yq(tickers, start, end):
     col_price = "adjclose" if "adjclose" in df.columns else "close"
     df = df[["symbol", "date", col_price]].dropna()
     df = df.rename(columns={col_price: "price"})
-    df["date"] = pd.to_datetime(df["date"]).dt.normalize()
+    df["date"] = pd.to_datetime(df["date"]).dt.tz_localize(fuso).dt.tz_convert(None)
 
     df = df.pivot(index="date", columns="symbol", values="price").sort_index()
 
